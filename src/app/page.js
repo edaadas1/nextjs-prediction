3946c1fea041ff2c2ba1af5092c61ec90bda7a6e
eda adas <eda.adas54@gmail.com>
2024-01-14 23:32:36 +0300

    Unusable files were cleaned and input button fields were created.

---
+++ src/app/page.js
@@ -0,0 +1,28 @@
+"use client"
+import { useRouter } from "next/navigation";
+import React, { useState } from "react"
+
+export default function Home() {
+
+  const [name, setName] = useState("");
+  const router = useRouter();
+
+  const predictionFunc = () => {
+    router.push(`/prediction/${name}`);
+  }
+
+
+  return (
+    <>
+      <div className="Card">
+        <div>
+          <h1>NAME PREDICTION</h1>
+        </div>
+        <div>
+          <input type="text" value={name} placeholder="Write your name..." style={{ outline: "none" }} onChange={(e) => setName(e.target.value)} ></input>
+          <button style={{ textDecoration: "none", color: "black" }} onClick={predictionFunc} >PREDICT</button>
+        </div>
+      </div>
+    </>
+  )
+}
