03200b515be3ff1a047d63b1c96aa4032672843d
eda adas <eda.adas54@gmail.com>
2024-01-15 00:14:18 +0300

    Homepage CSS edits were made

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 "use client"
 import { useRouter } from "next/navigation";
 import React, { useState } from "react"
+import styles from "./Home.module.css"
 
 export default function Home() {
 
@@ -13,16 +14,16 @@ export default function Home() {
 
 
   return (
-    <>
-      <div className="Card">
+    <div className={styles.body}>
+      <div className={styles.Card}>
         <div>
-          <h1>NAME PREDICTION</h1>
+          <h1 style={{ marginBottom: "40px" }}>NAME PREDICTION</h1>
         </div>
-        <div>
-          <input type="text" value={name} placeholder="Write your name..." style={{ outline: "none" }} onChange={(e) => setName(e.target.value)} ></input>
-          <button style={{ textDecoration: "none", color: "black" }} onClick={predictionFunc} >PREDICT</button>
+        <div style={{ display: "flex", gap: "20px" }}>
+          <input type="text" value={name} placeholder="Write your name..." style={{ outline: "none", padding: "10px" }} onChange={(e) => setName(e.target.value)} ></input>
+          <button style={{ textDecoration: "none", color: "black", padding: "10px" }} onClick={predictionFunc} >PREDICT</button>
         </div>
       </div>
-    </>
+    </div>
   )
 }
